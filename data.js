9a7b6c32ba4d23993075122203607a93680f2f53
Noximox <74255499+DaveAgustino@users.noreply.github.com>
2023-07-24 23:16:31 +0800

    upload files

---
+++ data.js
@@ -0,0 +1,48 @@
+const main = {
+  name: 'Dave Agustin A. Caacoy',
+  mail: 'caacoydave@gmail.com',
+  img: './profilepic.jpg',
+  role: ['Aspiring Full Stack Web Developer'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Home',
+      iconName: 'logo-firefox',
+      link: ''
+    },
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/DaveAgustino'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/davecaacoy/'
+    },
+    {
+      name: 'StackOverflow',
+      iconName: 'logo-stackoverflow',
+      link: 'https://stackoverflow.com/users/18845651/debb-codinglab'
+    },
+    {
+      name: 'Twitter',
+      iconName: 'logo-twitter',
+      link: 'https://twitter.com/Caacoydave'
+    },
+    {
+      name: 'npm',
+      iconName: 'logo-npm',
+      link: 'https://www.npmjs.com/~sivanesh_s'
+    },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'yourmail@gmail.com'}?Subject=Hello%20again`
+    }
+  ],
+  links: [
+    { name: 'Projects', link: '#projects' },
+    { name: 'Blogs', link: '#blogs' }
+  ]
+};
